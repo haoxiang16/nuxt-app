8e8b0594416c6c563bb2eb2103b8779ff272470f
haoxiang16 <81564640+haoxiang16@users.noreply.github.com>
2024-11-05 22:47:11 +0800

    nuxt app init

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>
+      Page: index
+    </h1>
+    <span>
+      目前路由的路徑-route.fullPath 為 : {{route.fullPath}}
+    </span>
+  </div>
+</template>
+
+<style scoped></style>
