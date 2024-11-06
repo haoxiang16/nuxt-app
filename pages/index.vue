1a831d7660b508619701769b35023ad6b5dcce6d
haoxiang16 <81564640+haoxiang16@users.noreply.github.com>
2024-11-06 22:33:50 +0800

    create  bootstrap style

--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,22 @@ const route = useRoute()
     <h1>
       Page: index
     </h1>
-    <span>
-      目前路由的路徑-route.fullPath 為 : {{route.fullPath}}
-    </span>
+    <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+$primary:blue;
+h1{
+  color: $primary;
+}
+</style>
